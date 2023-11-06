5567eee0fc8994c4e9ad2a5990a4bc300caed66d
Alex Malykh <49870662+ysomad@users.noreply.github.com>
2023-11-06 20:26:11 +0400

    add togglebit theme

--- lua/ysomad/plugins/colorscheme.lua
+++ lua/ysomad/plugins/colorscheme.lua
@@ -1,12 +1,12 @@
 return {
-  {
-    'aktersnurra/no-clown-fiesta.nvim',
-    priority = 1000,
-    config = function()
-      require("no-clown-fiesta").setup()
-      vim.cmd.colorscheme("no-clown-fiesta")
-    end
-  }
+  -- {
+  --   'aktersnurra/no-clown-fiesta.nvim',
+  --   priority = 1000,
+  --   config = function()
+  --     require("no-clown-fiesta").setup()
+  --     vim.cmd.colorscheme("no-clown-fiesta")
+  --   end
+  -- }
 
   -- "nyoom-engineering/oxocarbon.nvim",
   -- priority = 1000,
@@ -14,4 +14,13 @@ return {
   --   -- vim.cmd.colorscheme('no-clown-fiesta')
   --   vim.cmd.colorscheme('oxocarbon')
   -- end
+
+  {
+"elvessousa/sobrio",
+  priority = 1000,
+  config = function()
+    vim.cmd.colorscheme('sobrio')
+  end
+  }
+
 }
