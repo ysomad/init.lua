92d38f065fb397a07efdc48e37747c28de1e4972
Alex Malykh <49870662+ysomad@users.noreply.github.com>
2023-12-25 15:08:55 +0400

    colorizer: not use names

--- lua/ysomad/plugins/colorizer.lua
+++ lua/ysomad/plugins/colorizer.lua
@@ -1,5 +1,11 @@
 return {
   "NvChad/nvim-colorizer.lua",
   event = { "BufReadPre", "BufNewFile" },
-  config = true,
+  config = function ()
+    require('colorizer').setup({
+      user_default_options = {
+        names = false
+      }
+    })
+  end
 }
