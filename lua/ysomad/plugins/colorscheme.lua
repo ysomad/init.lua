199b07c5d29103ea64dd417cdcec7db09b92e611
Alex Malykh <49870662+ysomad@users.noreply.github.com>
2024-02-27 12:20:03 +0400

    fix colorscheme

--- lua/ysomad/plugins/colorscheme.lua
+++ lua/ysomad/plugins/colorscheme.lua
@@ -2,7 +2,7 @@ return {
   'aktersnurra/no-clown-fiesta.nvim',
   priority = 1000,
   config = function()
-    -- require("no-clown-fiesta").setup()
-    vim.cmd.colorscheme("togglebit")
+    require("no-clown-fiesta").setup()
+    vim.cmd.colorscheme("no-clown-fiesta")
   end
 }
