e54f5198749aa96af591cfc1eec98b8d03b0b161
Aleksei Malykh <49870662+ysomad@users.noreply.github.com>
2024-12-02 16:03:16 +0700

    center filename in statusbar

--- lua/plugins/statusline.lua
+++ lua/plugins/statusline.lua
@@ -5,11 +5,12 @@ return {
 			component_separators = { left = "", right = "" },
 			section_separators = { left = "", right = "" },
 			icons_enabled = false,
+			always_divide_middle = true,
 		},
 		sections = {
 			lualine_a = { "mode" },
 			lualine_b = { "branch", "diff", "diagnostics" },
-			lualine_c = { { "filename", path = 1 } },
+			lualine_c = { "%=", { "filename", file_status = true, path = 1 } },
 			lualine_x = { "filetype" },
 			lualine_y = {},
 			lualine_z = { "location" },
