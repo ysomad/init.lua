3de057c802b6e0714c51ab588946dfe960924b94
Aleksei Malykh <49870662+ysomad@users.noreply.github.com>
2024-12-02 15:49:50 +0700

    update statusline

--- lua/plugins/statusline.lua
+++ lua/plugins/statusline.lua
@@ -1,87 +1,21 @@
--- return {
---     'nvim-lualine/lualine.nvim',
---     dependencies = { 'nvim-tree/nvim-web-devicons' },
---     opts = {
---         options = {
---             component_separators = { left = '', right = '' },
---             section_separators = { left = '', right = '' },
---         },
---         sections = {
---             lualine_a = {'mode'},
---             lualine_b = {'branch', 'diff', 'diagnostics'},
---             lualine_c = {'filename'},
---             lualine_x = {'encoding'},
---             lualine_y = {'progress'},
---             lualine_z = {'location'}
---         },
---     },
---     config = function(_, opts)
---         require('lualine').setup(opts)
---     end
--- }
-
 return {
-	"tjdevries/express_line.nvim",
-	config = function()
-		local M = {}
-
-		local builtin = require("el.builtin")
-		local extensions = require("el.extensions")
-		local subscribe = require("el.subscribe")
-		local sections = require("el.sections")
-
-		vim.opt.laststatus = 3
-
-		M.setup = function()
-			require("el").setup({
-				generator = function()
-					local segments = {}
-
-					table.insert(segments, extensions.mode)
-					table.insert(segments, " ")
-					table.insert(
-						segments,
-						subscribe.buf_autocmd("el-git-branch", "BufEnter", function(win, buf)
-							local branch = extensions.git_branch(win, buf)
-							if branch then
-								return branch
-							end
-						end)
-					)
-					table.insert(
-						segments,
-						subscribe.buf_autocmd("el-git-changes", "BufWritePost", function(win, buf)
-							local changes = extensions.git_changes(win, buf)
-							if changes then
-								return changes
-							end
-						end)
-					)
-					table.insert(segments, function()
-						local task_count = #require("misery.scheduler").tasks
-						if task_count == 0 then
-							return ""
-						else
-							return string.format(" (Queued Events: %d)", task_count)
-						end
-					end)
-					table.insert(segments, sections.split)
-					table.insert(segments, "%f")
-					table.insert(segments, sections.split)
-					table.insert(segments, builtin.filetype)
-					table.insert(segments, "[")
-					table.insert(segments, builtin.line_with_width(3))
-					table.insert(segments, ":")
-					table.insert(segments, builtin.column_with_width(2))
-					table.insert(segments, "]")
-
-					return segments
-				end,
-			})
-		end
-
-		M.setup()
-
-		return M
+	"nvim-lualine/lualine.nvim",
+	opts = {
+		options = {
+			component_separators = { left = "", right = "" },
+			section_separators = { left = "", right = "" },
+			icons_enabled = false,
+		},
+		sections = {
+			lualine_a = { "mode" },
+			lualine_b = { "branch", "diff", "diagnostics" },
+			lualine_c = { { "filename", path = 1 } },
+			lualine_x = { "filetype" },
+			lualine_y = {},
+			lualine_z = { "location" },
+		},
+	},
+	config = function(_, opts)
+		require("lualine").setup(opts)
 	end,
 }
